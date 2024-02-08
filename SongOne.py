c3ff9ef46e8f92a648e422251d939ceefbb5fda8
Will Johnson <willj6294@gmail.com>
2024-02-08 01:50:37 -0600

    More Songs
    Added some other songs, with a chart each, if I've time, I'm going to make all the charts and maybe implement some of them.

---
+++ SongOne.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+import sys
+import time
+from Object import *
+from Still import *
+import os 
+import Official_Game
+import Object
+from pygame import mixer
+
+
+
+class Chart_Map:
+    
+
+    def load(map):
+        pass
+        #mixer.music.load(map)
+        #mixer.music.play()
+
+
+
