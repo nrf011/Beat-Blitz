e18409f029b22e6198254b2d056602a6847a0bb1
Will Johnson <willj6294@gmail.com>
2024-02-08 08:12:53 -0600

    Charts and finished function
    Function (Should for the most part)
    work, it takes the respective txt files for their respective songs and gets read, then it will (hopefully) display it as an arrow.

---
+++ Song_Base.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+import sys
+import time
+from Object import *
+from Still import *
+import os 
+import Official_Game
+import Object
+from pygame import mixer
+
+
+
+class Chart_Map:
+    
+
+    def load(map):
+        pass
+        #mixer.music.load(map + 'mp3')
+        #mixer.music.play()
+        #f = open(map + '.txt', 'r')
+        #data = f.readlines()
+    
+        #for y in range(len(data)):
+            #for x in range(len(data)):
+                #if data[y][x] == '0':
+                    #pygame.draw.up_arrow() #fix this in a min
+    
+#load(map)
+
+
